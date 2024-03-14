c266cf120445c272481b3c0748b5bd89a7273ab0
BurakcanA <burakcanaytac@gmail.com>
2024-03-14 17:21:22 +0300

    Get-Image-With-State

--- src/shared/Meme.jsx
+++ src/shared/Meme.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import data from "/public/data/memesData.js"
 
 export default function Meme() {
-    const memesArray = data.data.memes
+    const [allMemeImages,setAllMemeImages] = React.useState(data.data.memes)
+
+    const [meme,SetMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: ""
+    })
+    
+
     function getRandomPicture() {
         const rndNumber=Math.floor(Math.random() * memesArray.length)
-        const url =memesArray[rndNumber].url
-        console.log(url)
-        
+        SetMeme(item => ({
+            topText: "",
+            bottomText: "",
+            randomImage:allMemeImages[rndNumber].url
+        }))
     }
     return (
         <div className="container">
             <div className="form">
                 <div>
-                    <label htmlFor="top-text">Top Text</label>
                     <input id="top-text" className="form--input" placeholder="Top Text" type="text"></input>
                 </div>
                 <div>
-                    <label htmlFor="bottom-text">Bottom Text</label>
                     <input id="bottom-text" className="form--input" placeholder="Bottom Text" type="text"></input>
                 </div>
                     <button className="form--button" onClick={getRandomPicture}>Get a meme image 🖼️</button>
             </div>
-            <img id="meme--image"></img>
+            <img id="meme--image" className="meme--image" src={meme.randomImage}></img>
         </div>
     )
 }
